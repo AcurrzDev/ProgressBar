26acd4f1eefe925a6553b6903cfe837929e002e1
AcurrzDev <everlastingvoyageroblox@gmail.com>
2024-08-24 23:07:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 
 
 const fetchUsers = () => {
-    axios
-        .get('https://kick.com/api/v1/channels/fousey')
-        .then(response => {
-            fetch('./ChannelInfo.json').then(responses => {
-                responses.json().then(json => {
-                    let viewer_count = response.data.followersCount
-                    document.getElementById('view-count').textContent = `${viewer_count}/${json.goal}`
-                    let percent = (viewer_count/json.goal)*100
+    fetch('https://kick.com/api/v1/channels/fousey')
+        .then(resp => {
+            resp.json().then(response => {
+                fetch('./ChannelInfo.json').then(responses => {
+                    responses.json().then(json => {
+                        let viewer_count = response.followersCount
+                        console.log(viewer_count)
+                        document.getElementById('view-count').textContent = `${viewer_count}/${json.goal}`
+                        let percent = (viewer_count / json.goal) * 100
 
-                    console.log(percent)
+                        console.log(percent)
 
-                    if (percent>100){
-                        percent=100
-                    }
+                        if (percent > 100) {
+                            percent = 100
+                        }
 
-                    document.getElementById('progress-bar').style.width = `${percent}%`
+                        document.getElementById('progress-bar').style.width = `${percent}%`
+                    })
+
                 })
-
             })
         })
         .catch(error => console.error(error))
